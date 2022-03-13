0c8f2cfc0b280966f03a619ef1daca7ace7fd080
hsuan.lai <hsuan.lai@sincheng.net>
2022-03-13 15:09:31 +0800

    fix build go-rest container error

--- Plus/docker.sh
+++ Plus/docker.sh
@@ -3,12 +3,12 @@
 
 image_name="hsuan.cloud/hsuan/smartclouds-exercise-go"
 image_tag="20220313"
-
+cur_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 action=${1-}
 
 function build() {
   echo -e "\033[42;37mBuild Image: ${image_name}:${image_tag} Start\033[0m"
-  docker build -t ${image_name}:${image_tag} .
+  docker build -t ${image_name}:${image_tag} ${cur_dir}
   echo -e "\033[42;37mBuild Image: ${image_name}:${image_tag} End\033[0m"
 }
 
